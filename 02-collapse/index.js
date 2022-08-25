ca27380bb7bb2ef42e8dd5e2764487fbdcdb2cf4
klip96 <88762953+klip96@users.noreply.github.com>
2022-08-25 08:02:29 +0300

    fix: click in button

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,26 +1,24 @@
-/* 1 Рабочий вариант*/
-
 let But = document.getElementsByClassName('collapsible__button');
-document.getElementsByClassName('collapsible__action collapsible__action--hidden')[0].style.display='none';
+let hid = document.getElementsByClassName('collapsible__action collapsible__action--hidden')[0];
+let vis = document.getElementsByClassName('collapsible__action collapsible__action--visible')[0];
+let col = document.getElementsByClassName('collapsible__content')[0];
+col.style.display='none';
+vis.style.display='none';
 for (let i = 0; i < But.length; i++) {
     But[i].addEventListener('click', function(){
         this.classList.toggle('active');
         let content = this.nextElementSibling;
         if (this.classList.contains('active')) {
-            content.style.display = 'none';
-            document.getElementsByClassName('collapsible__action collapsible__action--visible')[0].style.display='none';
-            document.getElementsByClassName('collapsible__action collapsible__action--hidden')[0].style.display='block';
-        } else{
             content.style.display = 'block';
-            document.getElementsByClassName('collapsible__action collapsible__action--visible')[0].style.display='block';
-            document.getElementsByClassName('collapsible__action collapsible__action--hidden')[0].style.display='none';
-
+            vis.style.display='block';
+            hid.style.display='none';
+        } else{
+            content.style.display = 'none';
+            vis.style.display='none';
+            hid.style.display='block';
         }
-        
     })
 }
-
-/*------------------------------------------------------------------------------------------------------------------------------ */
 
 
 
